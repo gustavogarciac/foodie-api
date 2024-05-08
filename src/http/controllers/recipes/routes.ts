bbb6620ab37bf84e9d4b65ff48a500f7f289e783
gustavo <gustavogarciaci1@gmail.com>
2024-05-08 17:42:30 -0300

    refactor: changed the way to route

--- src/http/controllers/recipes/routes.ts
+++ src/http/controllers/recipes/routes.ts
@@ -1,28 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from "zod"
-import { createRecipe } from "./create-recipe";
+import { createRecipeRoute } from "./create-recipe";
 
 export async function recipesRoutes(app: FastifyInstance) {
-  app
-    .withTypeProvider<ZodTypeProvider>()
-    .post("/recipes", {
-      schema: {
-        summary: "Create a new recipe",
-        tags: ['recipes'],
-        body: z.object({
-          name: z.string().min(3).max(150),
-          description: z.string().min(3).max(500),
-          price: z.number().min(1),
-          totalDiscount: z.number().default(0),
-          imageUrl: z.string().url(),
-          categoryId: z.string().uuid()
-        }),
-        response: {
-          201: z.object({
-            recipeId: z.string().uuid()
-          })
-        }
-      }
-    }, createRecipe)
+ app.register(createRecipeRoute) 
 }
