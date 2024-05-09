bacd6b193fa7e0b46f123b780436e05974315dee
gustavo <gustavogarciaci1@gmail.com>
2024-05-09 20:38:41 -0300

    feat: prisma categories repository

---
+++ src/repositories/prisma/prisma-categories-repository.ts
@@ -0,0 +1,31 @@
+import { Prisma } from "@prisma/client";
+import { CategoriesRepository } from "../categories-repository";
+import { generateSlug } from "@/utils/generate-slug";
+import { prisma } from "@/lib/prismadb";
+
+export class PrismaCategoriesRepository implements CategoriesRepository {
+  async create(data: Prisma.CategoryCreateInput) {
+    const { name } = data
+    
+    const slug = generateSlug(name)
+
+    const category = await prisma.category.create({
+      data: {
+        name,
+        slug
+      }
+    })
+
+    return category
+  }
+
+  async findBySlug(slug: string) {
+    const category = await prisma.category.findFirst({
+      where: {
+        slug
+      }
+    })
+
+    return category
+  }
+}
